6026786a1acea388b14e6ff24592b2d37b675845
Pristine Judah <pristinejudah@Pristine-MacBook-Pro.local>
2023-05-06 07:42:22 +0100

    tested edamam api

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome VENCO React Native Hackathon</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +18,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import React, { useState, useEffect } from 'react';
+// import { View, Text, FlatList, Image } from 'react-native';
+// import { fetchRecipes } from './src/apis/testEdamamAPI';
+
+// const RecipeListScreen = () => {
+//   const [recipes, setRecipes] = useState([]);
+
+//   useEffect(() => {
+//     const loadRecipes = async () => {
+//       const data = await fetchRecipes('chicken');
+//       setRecipes(data);
+//     };
+//     loadRecipes();
+//   }, []);
+
+//   const renderItem = ({ item }: any) => {
+//     return (
+//       <View>
+//         <Image source={{ uri: item.recipe.image }} style={{ width: 100, height: 100 }} />
+//         <Text>{item.recipe.label}</Text>
+//         <Text>{item.recipe.calories}</Text>
+//       </View>
+//     );
+//   };
+
+//   return (
+//     <View>
+//       <FlatList data={recipes} renderItem={renderItem} keyExtractor={(item: any) => item.recipe.uri} />
+//     </View>
+//   );
+// };
+
+// export default RecipeListScreen;
